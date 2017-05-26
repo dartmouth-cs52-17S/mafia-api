ed2969ea4106e1ff64bfa1da6e50f9a545ad5444
SiaSyPeng <shiyao.peng.20@dartmouth.edu>
2017-05-25 23:23:23 -0400

    add heal/kill in backend

--- app/router.js
+++ app/router.js
@@ -34,4 +34,9 @@ router.post('/players', Player.createPlayers);
 
 router.get('/players/:id', Player.getPlayer);
 
+router.put('/players/kill/:id', Player.killPlayer);
+
+router.put('/players/heal/:id', Player.healPlayer);
+
+
 export default router;
